16993f812cd67df544714f1868f16a88378528b7
Morgan Driskell <morgandriskell@Morgans-MBP.lan>
2023-03-28 18:05:56 -0500

    working on the api

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,6 @@
-fetch("https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key")
+var APIKey = "914aa2f97745e35890164fd91e196191";
+
+fetch("https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={APIKey")
   .then(function(response) {
     return (response.json());
   });

848d4e2700e308f73fd396ca8aa73175383eacbb
Morgan Driskell <morgandriskell@Morgans-MBP.lan>
2023-03-30 10:57:35 -0500

    working on api key

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,22 @@
 var APIKey = "914aa2f97745e35890164fd91e196191";
+var city;
+var state;
+var country;
+var fetchButton = document.getElementById('search');
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=+{city}+&appid=+{APIKey}";
 
-fetch("https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={APIKey")
-  .then(function(response) {
+function getApi() {
+fetch(queryURL)
+  .then(function (response) {
     return (response.json());
+  })
+  .then(function (data) {
+    for (var i = 0; i < data.length; i++) {
+      var listItem = document.createElement('li');
+      listItem.textContent = data[i].html_url;
+      repoList.appendChild(listItem);
+    }
   });
+}
+
+fetchButton.addEventListener('click', getApi);

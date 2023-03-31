9820b7e541bed557a94ed220409677ad511997ea
Morgan Driskell <morgandriskell@Morgans-MBP.lan1>
2023-03-31 09:46:02 -0500

    working on api display

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,28 @@
+// variables to get the weather
 var APIKey = "914aa2f97745e35890164fd91e196191";
-var city;
-var state;
-var country;
+var cityEl = "atlanta";
 var fetchButton = document.getElementById('search');
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=+{city}+&appid=+{APIKey}";
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityEl + "&appid=" + APIKey;
+var units = "imperial";
 
+// ste up the funtion to grab the api
 function getApi() {
 fetch(queryURL)
   .then(function (response) {
     return (response.json());
   })
+//   logs all the data needed for the weather
+  .then(function (data) {
+    console.log(data)
+    
+  })
   .then(function (data) {
     for (var i = 0; i < data.length; i++) {
       var listItem = document.createElement('li');
       listItem.textContent = data[i].html_url;
-      repoList.appendChild(listItem);
+      weather.appendChild(listItem);
     }
   });
-}
+};
 
 fetchButton.addEventListener('click', getApi);

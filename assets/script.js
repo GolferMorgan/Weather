df82e2884d1f233665a2cfc5d98f4d1839774078
Morgan Driskell <morgandriskell@Morgans-MBP.lan>
2023-04-01 12:07:45 -0500

    working on displaying api data

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,12 @@
 // variables to get the weather
 var APIKey = "914aa2f97745e35890164fd91e196191";
 var cityEl = "atlanta";
-var fetchButton = document.getElementById('search');
+var temp = document.getElementById("temp");
+var humidity = document.getElementById("humidity")
+var wind = document.getElementById("wind")
+var weather = document.getElementById("weather")
+var description = document.getElementById("description")
+var search = document.getElementById("btn");
 var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityEl + "&appid=" + APIKey;
 var units = "imperial";
 
@@ -13,20 +18,14 @@ fetch(queryURL)
   })
 // logs all the data needed for the weather
   .then(function (data) {
-    console.log(data)
-    return (data)
-  })
-  .then(function (data) {
-    console.log(data.main)
-    return (data.main)
-  })
-  .then(function (data) {
-    console.log(data.base)
-    return (data.base)
+    // displays current weather
+    renderCurrentWeather(data).setAttribute("weather")
+    return (data) 
   })
 };
 
-// need to add a way to display data for current weather
+function renderCurrentWeather()
+
 
 // need to add a way to display 5 day outlook
 
@@ -36,4 +35,4 @@ fetch(queryURL)
 
 
 // added event listerner to the search button 
-fetchButton.addEventListener('click', getApi);
+search.addEventListener('click', getApi);

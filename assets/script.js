f95cea9642e6f72fb355ccb3718cdab3d061c54c
Morgan Driskell <morgandriskell@Morgans-MBP.lan>
2023-04-02 21:33:20 -0500

    added the other fields for current weather

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,6 @@ var humidity = document.getElementById("humidity")
 var wind = document.getElementById("wind")
 var weather = document.getElementById("weather")
 var outlook = document.getElementById("outlook")
-var description = document.getElementById("description")
 var search = document.getElementById("btn");
 var units = "imperial";
 var locations = []
@@ -22,48 +21,71 @@ fetch(queryURL)
 // logs all the data needed for the weather
   .then(function (data) {
     // displays current weather
-    renderCurrentWeather(data)
+    renderWeather(data)
     console.log(data)
     return (data) 
   })
   .then(function (data) {
-    renderWind(data.main)
-    console.log(data.main)
+    renderWind(data)
+    console.log(data.wind)
+    return (data.wind)
+  })
+  .then(function (data) {
+    renderHumidity(data)
+    console.log(data.humidity)
     return (data.main)
-});
-}
+  })
+  .then(function (data) {
+    renderTemp(temp)
+    console.log(data.temp)
+    return (temp)
+  });
 
 // current weather
-function renderCurrentWeather(data) {
+function renderWeather(data) {
    weather.innerHTML = data.weather[0].description
      return (data);
 }    
 
 // current wind
 function renderWind(data) {
-  wind.innerhtml = data.main[0]
-  return (data.main);
+  wind.innerHTML = data.wind
+  return (wind);
 }
 
+// current humidity
+function renderHumidity(data) {
+  console.log(data.humidity)
+  humidity.innerHTML = data.humidity
+  return (humidity);
+}
 
-function getForecast() {
-   var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={APIKey}";
-   fetch(forecastURL)
-   .then (function (response) {
-   return (response.json());
-  })
-   .then(function (data) {
-   renderForecast(data)
-   console.log(data)
-   return (data)
-  })
-  }
+// current temp
+function renderTemp(data) {
+  console.log(data.temp)
+  temp.innerHTML = data.temp
+  return (temp);
+}
+}
+
+// function getForecast() {
+//    var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={APIKey}";
+//    fetch(forecastURL)
+//    .then (function (response) {
+//    return (response.json());
+//   })
+//    .then(function (data) {
+//    renderForecast(data)
+//    console.log(data)
+//    return (data)
+//   })
+//   }
     
-function renderForecast(data) {
-  var outlook = getElementById("outlook")
-   outlook.innerHTML = data[0].description
-   return (data);
-  }
+// function renderForecast(data) {
+//   var outlook = getElementById("outlook")
+//    outlook.innerHTML = data[0].description
+//    return (data);
+//   }
   
 
 // function renderCurrentWeather(data) {
@@ -79,9 +101,9 @@ function renderForecast(data) {
 // need to add the searched locations under previous locations tab
 function appendSearchHistory() {
     // added local storage so it would save the location
-    localStorage.setItem('locations', input);
+    localStorage.setItem("btn", input-bar);
     // set alert to show the appointment was saved
-    console.log(locations);
+    console.log(input-bar);
 }
 
 // need to add local storage so everything stays on page
@@ -91,3 +113,4 @@ function getSearchHistory() {
 
 // added event listerner to the search button 
 search.addEventListener('click', getApi);
+localStorage.setItem("btn", input-bar)
